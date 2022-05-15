840ad75f61c045f8b716f61dba4a46f615bc3e1b
zevleoz <61064016+zevleoz@users.noreply.github.com>
2022-05-15 16:37:04 -0500

    find host

--- goldenglobewinners.py
+++ goldenglobewinners.py
@@ -4,6 +4,7 @@ import json
 import nltk
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
+from nltk import pos_tag
 
 
 def read_tweets():
@@ -19,8 +20,41 @@ def read_tweets():
 
 tweets = read_tweets()
 
-for tweet in tweets:
-    words = word_tokenize(tweet)
-    if 'won' in words:
-        index = words.index('won')
-        # print(words[index:])
+#dictionary for hosts
+hostName = dict()
+def addToDict(key,dictionary):
+    if key in dictionary:
+        dictionary[key] += 1
+    else:
+        dictionary[key] = 1
+
+#final list output of hosts
+hosts = []
+#regex pattern for hosts
+hostRegEx = ['host','hosts','hosted','hosting']
+hostJointRegEx = '|'.join(hostRegEx)
+hostre = re.compile(hostJointRegEx, re.IGNORECASE)
+
+#find hosts
+def findHosts(tweets):
+    for tweet in tweets:
+        if hostre.search(tweet):
+            unigram = word_tokenize(tweet)
+            bigrams = nltk.bigrams(unigram)
+            for bigram in bigrams:
+                numNames = 0
+                POStag = nltk.pos_tag(bigram)
+                for (text, tag) in POStag:
+                    if tag == 'NNP': #tag for proper noun
+                        numNames += 1
+                if numNames ==2:
+                    name = "%s %s" % bigram
+                    addToDict(name, hostName)
+temp = 0
+for key in hostName:
+    if hostName[key] > temp:
+        temp = hostName[key]
+for key in hostName:
+    if temp < 1.05 * hostName[key]:
+        hosts.append(key) 
+print(hosts)
