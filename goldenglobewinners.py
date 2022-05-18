16e104edff82724745f0b1a1fdee77cc20365d7b
zevleoz <61064016+zevleoz@users.noreply.github.com>
2022-05-18 02:06:42 -0500

    find awards working

--- goldenglobewinners.py
+++ goldenglobewinners.py
@@ -111,5 +111,43 @@ for key in hostName:
 for key in hostName:
     if temp == hostName[key]:
         hosts.append(key) 
-print(hostName)
-print(hosts)
+#host name stored in hosts. if there is only one host, their name will be at hosts[0]
+
+def findAwards():
+    counter= {}
+    awards = []
+    endwords = ['picture','drama','film','musical','television']
+    trash  = []
+    for x in filteredTweets:
+        tweet = x.split()
+        for endword in endwords:
+            if endword in tweet and 'best' in tweet:
+                s = tweet.index('best')
+                e = tweet.index(endword)
+                if s < e:
+                    award = " ".join(tweet[s:e+1])
+                    if award in counter:
+                        counter[award] += 1
+                    else:
+                        counter[award] = 1
+    for key in counter:
+        if counter.get(key) > 6:
+            award = key
+            if len(award.split())>3:
+                awards.append(award)
+    for i in range(0,len(awards)-1):
+        for j in range(i+1, len(awards)):
+            a1 = awards[i]
+            a2 = awards[j]
+            if (len(a1)>len(a2) and a2 in a1):
+                trash.append(a2)
+            if (len(a2)>len(a1) and a1 in a2):
+                trash.append(a1)
+    for x in trash:
+        if x in awards:
+            awards.remove(x)
+
+    return awards[0:24]
+
+#stored 25 awards outputed by the parser
+awards = findAwards()
