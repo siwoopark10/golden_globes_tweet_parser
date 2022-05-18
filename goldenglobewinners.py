9d7364e6e98d5b3614b181e27e1fe39793693d8d
zevleoz <61064016+zevleoz@users.noreply.github.com>
2022-05-18 01:28:18 -0500

    host name working

--- goldenglobewinners.py
+++ goldenglobewinners.py
@@ -2,14 +2,18 @@ import os
 import re
 import json
 import nltk
+import spacy
+model = spacy.load('en_core_web_sm')
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
-
-
+from nltk.tokenize import TreebankWordTokenizer
+wordTokenizer = TreebankWordTokenizer()
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
 def read_tweets():
     cwd = os.getcwd()
-    path = cwd + '\\gg2013.json\\gg2013.json'
+    path = cwd + '/gg2013.json'
     f = open(path)
 
     tweet_json = json.load(f)
@@ -20,6 +24,21 @@ def read_tweets():
 
 tweets = read_tweets()
 
+filterRegExPatterns = ['host?[s]', 'hosting', 'hosted', 'won best', 'winner', 'wins', 'presented', 'presenting', 'red carpet', 'best dressed', 'best dress', 
+'worst dressed', 'worst dress'] 
+filterRegExPatternJoin = "|".join(filterRegExPatterns)
+filterRegEx = re.compile(filterRegExPatternJoin, re.IGNORECASE)
+#re ex for removing all the rt
+filterRetweetRegEx = re.compile('^RT', re.IGNORECASE)
+filteredTweets = []
+count = 0
+for tweet in tweets:
+    if(filterRegEx.search(tweet) and not filterRetweetRegEx.search(tweet)):
+        count += 1
+        tweetCleaned = re.sub(r'[^a-zA-Z0-9 ]',r'',tweet)
+        filteredTweets.append(tweetCleaned)
+blacklist = ['golden', 'globes', 'goldenglobes', '#goldenglobes', '#golden']
+stop.append(blacklist)
 #dictionary for hosts
 hostName = dict()
 def addToDict(key,dictionary):
@@ -35,26 +54,62 @@ hostRegEx = ['host','hosts','hosted','hosting']
 hostJointRegEx = '|'.join(hostRegEx)
 hostre = re.compile(hostJointRegEx, re.IGNORECASE)
 
+def identify_entities(text, stop_words):
+    tags = {}
+    for ent in model(text).ents:
+        entity = ent.text.strip()
+        if entity not in tags and len(entity) > 1:
+            # remove stopwords
+            entity_split = [w for w in entity.split() if w.lower() not in stop_words]
+            if len(entity_split) != 0:
+                entity = ' '.join(entity_split)
+                # if entity is a single word that is 'the' or is a single character
+                if (len(entity.split()) == 1) or len(entity) == 1:
+                    pass
+                else:
+                    tags[entity]=[ent.label_]
+    return tags
 #find hosts
 def findHosts(tweets):
-    for tweet in tweets:
-        if hostre.search(tweet):
-            unigram = word_tokenize(tweet)
-            bigrams = nltk.bigrams(unigram)
-            for bigram in bigrams:
-                numNames = 0
-                POStag = nltk.pos_tag(bigram)
-                for (text, tag) in POStag:
-                    if tag == 'NNP': #tag for proper noun
-                        numNames += 1
-                if numNames ==2:
-                    name = "%s %s" % bigram
-                    addToDict(name, hostName)
+    entity_freq_dict = {}
+
+    for line in tweets:
+        match = re.search(hostre, line)
+        if match:
+            for entity in identify_entities(line, stop).keys():
+                if entity not in entity_freq_dict:
+                    entity_freq_dict[entity] = 1
+                else:
+                    entity_freq_dict[entity] += 1
+    return entity_freq_dict
+    #for tweet in tweets:
+     #       filterText = ' '.join(word for word in tweet.split() if word.lower() not in stop and word.lower() not in blacklist)
+      #      unigram = wordTokenizer.tokenize(filterText)
+       #     posTag = nltk.pos_tag(unigram)
+        #    
+         #   for(data, tag) in posTag:
+
+                    #Check for proper nouns
+          #      if tag == 'NNP':
+                     
+           #         addToDict(data, hostName)
+                #if both the words in bigram are proper nouns, mark the bigram as probable host name        
+
+                    
+                        #numNames += 1
+                
+            #if numNames == 2:
+                   
+                 #   name = "%s %s" % bigram
+                    
+                 #   addToDict(name, hostName)
+hostName  = findHosts(filteredTweets)
 temp = 0
 for key in hostName:
     if hostName[key] > temp:
         temp = hostName[key]
 for key in hostName:
-    if temp < 1.05 * hostName[key]:
+    if temp == hostName[key]:
         hosts.append(key) 
+print(hostName)
 print(hosts)
